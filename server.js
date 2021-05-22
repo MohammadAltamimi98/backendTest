037914afdedce54cca3916c9f8d83522adba4e6a
Rula Al-qasem <jaradatrula6@gmail.com>
2021-05-22 21:03:47 +0300

    art

--- server.js
+++ server.js
@@ -3,21 +3,22 @@ const app = express() // initialize your express app instance
 const handleMovie = require('./modules/movies')
 const cors = require('cors')
 const handleNews = require('./modules/news');
+const handleArt = require('./modules/art');
 
 
 
- 
+
 app.use(cors());
 
 // a server endpoint 
 app.get('/', // our endpoint name
- function (req, res) { // callback function of what we should do with our request
-  res.send('Hello World') // our endpoint function response
-})
+  function (req, res) { // callback function of what we should do with our request
+    res.send('Hello World') // our endpoint function response
+  })
 
 
 app.get('/movie', handleMovie)
-
+app.get('/art', handleArt)
 app.get('/news', handleNews)
- 
+
 app.listen(3009) // kick start
